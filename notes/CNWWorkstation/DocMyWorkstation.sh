754d186bf9b8d9b6d7ae86ed41a706331767b783
Charles Wyble <charles@turnsys.com>
2020-10-10 22:26:24 -0500

    another savepoint, cause i did stuff and stuff

--- notes/CNWWorkstation/DocMyWorkstation.sh
+++ notes/CNWWorkstation/DocMyWorkstation.sh
@@ -3,9 +3,10 @@
 
 DOCS_DATE="$(date +%m-%d-%Y)"
 
-mkdir $DOCS_DATE
-mkdir $DOCS_DATE/sources-list-save
+mkdir "$DOCS_DATE"
+mkdir "$DOCS_DATE"/sources-list-save
 
-dpkg --get-selections > $DOCS_DATE/InstalledPackaes
-rsync -av /etc/apt/sources.list* $DOCS_DATE/sources-list-save
-docker images|awk '{print $1" "$2}' > $DOCS_DATE/dockerImagesList
+dpkg --get-selections > "$DOCS_DATE"/InstalledPackaes
+rsync -av /etc/apt/sources.list* "$DOCS_DATE"/sources-list-save
+docker images|awk '{print $1" "$2}' > "$DOCS_DATE"/dockerImagesList
+code --list-extensions > "$DOCS_DATE"/vscodeExtensions
